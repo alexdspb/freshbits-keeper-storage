1b5e78b90794dadd98f3ab0e08827fd97d14bede
Alex Danilin <alexander.danilin@dataart.com>
2020-01-15 13:28:24 +0300

    App dockerized

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(cors());
 app.use('/api/v1', require('./routes/notebooks'));
 app.use('/api/v1', require('./routes/notes'));
 
+const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, HOST);
+console.log(`Server running in ${process.env.NODE_ENV} mode on ${HOST}:${PORT}`);
